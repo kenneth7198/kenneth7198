69551e25cc16b171fdb9a30dd37df8603ed40425
Kenneth Liu <995877+kenneth7198@users.noreply.github.com>
2024-10-28 00:24:04 +0800

    Update LED_PWM.ino

--- LED_PWM/LED_PWM.ino
+++ LED_PWM/LED_PWM.ino
@@ -1,15 +1,19 @@
 int readAnalog;
 int analogPin = 0;
-int ledPin = 9;
+int ledPin = 10;
 void setup() {
  Serial.begin(9600);
  pinMode(ledPin, OUTPUT);
 }
- 
+
 void loop() {
   readAnalog = analogRead(analogPin);
-  //將擷取到的類比訊號用map函式轉換成LED的PWM輸出
-  int pwm = map(readAnalog, 0, 1023, 0, 255);
+  Serial.print("Aanlog Input:");
+  Serial.println(readAnalog);
+  //運用LED的PWM輸出讓LED具備漸暗與漸亮效果
+  //運用map函式來轉換原本輸入的類比訊號，轉換成PWM輸出0~255
+  int pwm = map(readAnalog, 0, 1023, 0, 255); 
+  //運用analogWrite函式控制LED的PWM輸出
   analogWrite(ledPin, pwm);
   delay(100);
 }
